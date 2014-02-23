9074ece3b8c4fcf2cc584754bd7cb5fa86668d67
himangshu <himangshu@sokratik.com>
2014-02-23 17:20:59 +0530

    1st commit
    Signed-off-by: himangshu <himangshu@sokratik.com>

---
+++ src/presentation_generator/core.clj
@@ -0,0 +1,7 @@
+(ns presentation-generator.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/presentation_generator/core_test.clj
@@ -0,0 +1,7 @@
+(ns presentation-generator.core-test
+  (:require [clojure.test :refer :all]
+            [presentation-generator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
