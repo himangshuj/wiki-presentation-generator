1d3114f85023f2bf0de5b67cd378b7dd976363bf
himangshu <himangshu@sokratik.com>
2014-02-23 17:23:09 +0530

    Adding project description and url
    Signed-off-by: himangshu <himangshu@sokratik.com>

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject presentation-generator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Given a sequence of images and text it creates a sokratik presentation"
+  :url "http://lab.sokratik.com"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main ^:skip-aot presentation-generator.core
   :target-path "target/%s"
